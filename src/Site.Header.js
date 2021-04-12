5b6520fb7fac7eb83b91e0f7428ec7dfffc79218
Ben Smith <ben@bensmithsound.uk>
2021-04-12 20:26:08 +0100

    Changed format of header

--- src/Site.Header.js
+++ src/Site.Header.js
@@ -4,12 +4,13 @@ function SiteHeader() {
   return (
     <div className="container-fluid header">
       <div className="row pt-3 pb-2 justify-content-center align-items-center">
-        <div className="col-lg-3">
-          <p className="header-title display-4">Ben Smith</p>
+        <div className="col-lg-auto">
+          <p className="header-title display-4 mb-0">Ben Smith</p>
+          <p className="header-subtitle mt-0">Theatre Sound Engineer and Designer</p>
         </div>
-        <div className="col-lg-4">
+        {/*<div className="col-lg-4">
           <p className="header-subtitle">Theatre Sound Engineer and Designer</p>
-        </div>
+  </div>*/}
       </div>
     </div>
   );
